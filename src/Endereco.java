70e9c900006ba3987de5da665c290f3e817b1a6f
Emerson <emerson_pariz@hotmail.com>
2024-06-04 21:08:08 -0300

    Criado as primeiras classes java da etapa um.

---
+++ src/Endereco.java
@@ -0,0 +1,91 @@
+public class Endereco {
+    private String nomeDaRua;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String cep;
+    private String numero;
+    private String complemento;
+
+    public Endereco(String nomeDaRua, String cidade, String estado, String pais, String cep, String numero, String complemento) {
+        //inicializando os atributos da classe
+        this.nomeDaRua = nomeDaRua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    public String getNomeDaRua() {
+        return nomeDaRua;
+    }
+
+    public void setNomeDaRua(String nomeDaRua) {
+        this.nomeDaRua = nomeDaRua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public String toString() {
+        //TODO: The return type is incompatible with Object.toString()Java(67109268) 
+        //represetação em forma de string da classe
+        return "Endereco{" +
+                "nomeDaRua='" + nomeDaRua + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", estado='" + estado + '\'' +
+                ", pais='" + pais + '\'' +
+                ", cep='" + cep + '\'' +
+                ", numero='" + numero + '\'' +
+                ", complemento='" + complemento + '\'' +
+                '}';
+    }
+}
---
+++ src/Loja.java
@@ -0,0 +1,71 @@
+public class Loja {
+    // (visibilidade tipo etiqueta) = atributo
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        //primeiro método construtor
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    // TODO: e se converter os argumentos?
+    public Loja(String nome, int quantidadeFuncionarios) {
+        //segundo método construtor, usando uma categoria do polimorfismo chamado sobrecarga.
+        this(nome, quantidadeFuncionarios, -1);
+    }
+
+    public String getNome() {
+        // getter
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        // setter
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    @Override
+    public String toString() {
+        return "Loja{" +
+                "nome='" + nome + '\'' +
+                ", quantidadeFuncionarios=" + quantidadeFuncionarios +
+                ", salarioBaseFuncionario=" + salarioBaseFuncionario +
+                '}';
+    }
+
+    public double gastosComSalario() {
+        if (salarioBaseFuncionario == -1) {
+            return -1;
+        }
+        return quantidadeFuncionarios * salarioBaseFuncionario;
+    }
+
+    public char tamanhoDaLoja() {
+        if (quantidadeFuncionarios < 10) {
+            return 'P';
+        } else if (quantidadeFuncionarios <= 30) {
+            return 'M';
+        } else {
+            return 'G';
+        }
+    }
+}
